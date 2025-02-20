f137f294fb5dd492282302412c3d389a537d0e97
SabaSiva2916 <sabasiva2916@gmail.com>
2025-02-20 08:31:57 +0530

    general code created

---
+++ src/org/sample/Day1.java
@@ -0,0 +1,39 @@
+package org.sample;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+
+		// Mention the browser
+		WebDriver driver = new ChromeDriver(); // TO open chrome browser
+		// WebDriver --> Interface
+		// ChromeDriver ---> Class
+		// driver --> It is object reference of ChromeDriver class
+
+		// Loading the URL
+		driver.get("https://www.facebook.com/");
+		// get() --> which is present on WebDriver interface
+
+		// TO get the tilte of the Webpage
+		String tile = driver.getTitle();
+		System.out.println(tile);
+
+		// TO get the url after lauch
+	
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		
+		//WedDriver -> Interface
+		   //get() --> to launch the browser
+		 //getTitle() --> used to get the title of the Webpage
+		 //getCurrentUrl() --> used to get the url of the Webpage
+//8015908440 
+		//Sabapathi S
+		
+		//Name, Number -- attend selenium class
+	}
+}
