aab0390c1297a210bc035145f5a1c56e4fbda5d3
SabaSiva2916 <sabasiva2916@gmail.com>
2025-02-21 08:34:05 +0530

    dev team push

--- src/org/sample/Day1.java
+++ src/org/sample/Day1.java
@@ -1,7 +1,11 @@
 package org.sample;
 
+import java.util.List;
+
 import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Day1 {
@@ -23,17 +27,22 @@ public class Day1 {
 		System.out.println(tile);
 
 		// TO get the url after lauch
-	
+
 		String currentUrl = driver.getCurrentUrl();
 		System.out.println(currentUrl);
-		
-		//WedDriver -> Interface
-		   //get() --> to launch the browser
-		 //getTitle() --> used to get the title of the Webpage
-		 //getCurrentUrl() --> used to get the url of the Webpage
-//8015908440 
-		//Sabapathi S
-		
-		//Name, Number -- attend selenium class
+
+		// WedDriver -> Interface
+		// get() --> to launch the browser
+		// getTitle() --> used to get the title of the Webpage
+		// getCurrentUrl() --> used to get the url of the Webpage
+		// 8015908440
+		// Sabapathi S
+
+		// Name, Number -- attend selenium class
+		List<WebElement> allLinks = driver.findElements(By.xpath("//a[@href='links']"));
+		for (WebElement ele : allLinks) {
+			ele.click();
+		}
 	}
+
 }
